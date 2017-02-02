02ffd7afcb05bd3192c8fff00eb4df22da1d9803
hedda <hedda@192.168.0.13>
2017-02-02 13:39:29 +0100

    Az 5%-ra az also hatarellenorzest is beleraktam, illetve figyeli, hogy ha nem valtozott az uj fizetesi osszeg a korabbihoz kepest, akkor ne legyen mentes, csak figyelmeztetes. Spinner ertekek beallitasa is kesz.

--- src/nezet/AdatBekeres.java
+++ src/nezet/AdatBekeres.java
@@ -28,7 +28,7 @@ public class AdatBekeres extends JDialog {
   private JButton btOK = new JButton("Mehet");
 
 
-  public AdatBekeres(JFrame tulajdonos, Dolgozo dolgozo) {
+  public AdatBekeres(JFrame tulajdonos, Dolgozo dolgozo, AdatBazisKezeles modell) {
     super(tulajdonos, "Adat bekérés", true);
 
     setLayout(new BorderLayout());
@@ -40,12 +40,24 @@ public class AdatBekeres extends JDialog {
     setSize(300, 300);
 
     setLocationRelativeTo(tulajdonos);
+    int minFizetes = modell.lekerdezMinFizetes(dolgozo.getMunkakor());
+    int maxFizetes = modell.lekerdezMaxFizetés(dolgozo.getMunkakor());
+    int aktFizetes = dolgozo.getFizetes();
+    int emeles5szazalek = Math.round(aktFizetes*1.05F);
+    int csokkentes5szazalek = Math.round(aktFizetes*0.95F);
+    int adhatoMax = maxFizetes>emeles5szazalek?emeles5szazalek:maxFizetes;
+    int adhatoMin = minFizetes<csokkentes5szazalek?csokkentes5szazalek:minFizetes;
+    System.out.println("aktfizu: "+aktFizetes+", 5% plusz: "+emeles5szazalek+
+            ", 5% minusz: "+csokkentes5szazalek+", maxfizu: "+maxFizetes+", min fizu: "+minFizetes+
+            ", adhatoMax: "+adhatoMax+", adhatoMin:. "+adhatoMin);
+    
     JPanel pn = new JPanel(new GridLayout(5, 1));
     JLabel lbdolgozNev = new JLabel( "Dolgozó neve:             "+dolgozo.getNev());
     JLabel lbFizetes = new JLabel(   "Dolgozó fizetése:         "+dolgozo.getFizetes());
-    JLabel lbMaxFizetes = new JLabel("Adható maximális fizrtés: "+dolgozo.getMaxFizetés());
-    JLabel lbMinFizetes = new JLabel("Adható minimális fizetés: "+dolgozo.getMinFizetes());
-    JSpinner sp=new JSpinner(new SpinnerNumberModel(dolgozo.getFizetes(), dolgozo.getMinFizetes(), dolgozo.getMaxFizetés(), 100));
+    JLabel lbMaxFizetes = new JLabel("Adható maximális fizetés: "+maxFizetes);
+    JLabel lbMinFizetes = new JLabel("Adható minimális fizetés: "+minFizetes);
+    
+    JSpinner sp=new JSpinner(new SpinnerNumberModel(aktFizetes, adhatoMin, adhatoMax, 50));
     pn.add(lbdolgozNev);
     pn.add(lbFizetes);
     pn.add(lbMaxFizetes);
@@ -57,14 +69,13 @@ public class AdatBekeres extends JDialog {
       @Override
       public void mouseClicked(MouseEvent e) {
         System.out.println("Dolgozo azonositoja :"+dolgozo.getEmpID()+" uj fizu: "+(int)sp.getModel().getValue() );
-        try {
-          AdatBazisKezeles.modositFizetés(dolgozo.getEmpID(), (int)sp.getModel().getValue());
+        //Ha nem valtozott a fizu osszeg, akkor NE mentsuk!
+        if ((int)sp.getModel().getValue()!=aktFizetes) {
+          boolean siker = AdatBazisKezeles.modositFizetés(dolgozo.getEmpID(), (int)sp.getModel().getValue());
           dolgozo.setFizetes((int)sp.getModel().getValue());
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-       //   Logger.getLogger(AdatBekeres.class.getName()).log(Level.SEVERE, null, ex);
         }
-        JOptionPane.showMessageDialog((Component) e.getSource(), "Itt kéne adatbázisba írni", "Írás", JOptionPane.INFORMATION_MESSAGE);
+        else
+          JOptionPane.showMessageDialog((Component) e.getSource(), "Ugyanarra nem lehet módosítani a fizetést, ami\n korábban volt!", "Figyelmeztetés", JOptionPane.ERROR_MESSAGE);
         dispose();
       }
     });
